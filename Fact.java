3ad8392a30b2dac4c19260b361a5b393a6d97163
divi2312 <68528992+divi2312@users.noreply.github.com>
2020-09-08 16:45:46 +0530

    Add files via upload

---
+++ Fact.java
@@ -0,0 +1,15 @@
+
+package fact;
+import java.io.*;
+
+public class Fact {
+    void display()
+    {
+        System.out.println("abcd");
+    }
+    
+    public static void main(String[] args) {
+      new Fact().display();
+    }
+    
+}
